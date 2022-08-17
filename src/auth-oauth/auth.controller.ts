3e5fa0c3e4dfc4f80ce4b695848b947f260638c4
Viktoriia <vikabobir32@gmail.com>
2022-08-17 17:30:23 +0300

    change 'google' to 'auth' in auth.controller.ts

--- src/auth-oauth/auth.controller.ts
+++ src/auth-oauth/auth.controller.ts
@@ -4,7 +4,7 @@ import { Request, Response } from 'express';
 
 import { AuthService } from './auth.service';
 
-@Controller('google')
+@Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
